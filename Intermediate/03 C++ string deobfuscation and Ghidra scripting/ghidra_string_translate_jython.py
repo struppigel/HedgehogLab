d83c17f29432f5c6daf4c634fdbfe4b17c4023e1
struppigel <struppigel@googlemail.com>
2025-08-03 05:26:21 +0200

    added string translate script for string deobfuscation section

---
+++ Intermediate/03 C++ string deobfuscation and Ghidra scripting/ghidra_string_translate_jython.py
@@ -0,0 +1,31 @@
+# @category Strings
+
+from ghidra.program.model.data import StringDataInstance, TranslationSettingsDefinition
+from ghidra.program.util import DefinedDataIterator
+from ghidra.app.script import GhidraScript
+from util import CollectionUtils
+
+def translate_string(s):
+    # customize here
+    return "TODO " + s + " TODO"
+
+count = 0
+listing = currentProgram.getListing()
+num_defined_data = listing.getNumDefinedData()
+
+monitor.initialize(num_defined_data)
+monitor.setMessage("Translating strings")
+
+for data in CollectionUtils.asIterable(DefinedDataIterator.definedStrings(currentProgram, currentSelection)):
+    if monitor.isCancelled():
+        break
+    str_data_instance = StringDataInstance.getStringDataInstance(data)
+    s = str_data_instance.getStringValue()
+    
+    if s is not None:
+        TranslationSettingsDefinition.TRANSLATION.setTranslatedValue(data, translate_string(s))
+        TranslationSettingsDefinition.TRANSLATION.setShowTranslated(data, True)
+        count += 1
+        monitor.incrementProgress(1)
+
+println("Translated " + str(count) + " strings.")
